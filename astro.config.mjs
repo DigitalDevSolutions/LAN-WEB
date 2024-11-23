236e9b54294e2fcdebdd0738f040205ff271ef3e
niba291 <nibachavez291@gmail.com>
2024-11-23 19:18:21 -0300

    V1.0.6: Add robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-article.netlify.app/',
-    integrations: [
+  site: "https://digitaldevsolutions.cl",
+  integrations: [
+        sitemap(),
         tailwind(),
         partytown({
             config: {
